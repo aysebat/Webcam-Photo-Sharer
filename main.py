947386b3fbec021895d182318d8d9549a7fa36d0
aysebat <aaysebatt@gmail.com>
2023-01-08 21:07:15 +0300

    added the share link properties

--- main.py
+++ main.py
@@ -8,21 +8,32 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
+        """Starts camera and changes Button text"""
         self.ids.camera.play = True
         self.ids.start.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
     def stop(self):
+        """Stops camera and changes Button text"""
         self.ids.camera.play = False
         self.ids.start.text = 'Start Camera'
         self.ids.camera.texture = None
     def capture(self):
+        """Create a filename with current time and captures
+        and saved a photo image under that filename"""
         currentTime = time.strftime('%Y%m%d-%H%M%S')
-        filepath =f"images/{currentTime}.png"
-        self.ids.camera.export_to_png(filepath)
+        self.filepath = f"images/{currentTime}.png"
+        self.ids.camera.export_to_png(self.filepath)
         self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = self.filepath
 
 class ImageScreen(Screen):
-    pass
+    def create_link(self):
+        filepath = App.get_running_app().root.ids.camera_screen.filepath
+        fileshare = FileSharer(filepath=filepath)
+        url = fileshare.share()
+        self.ids.link.text = url
+
+
 
 
 class RootWidget(ScreenManager):
