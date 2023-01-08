9590389f21650a7c5ff505ea17304e777cbb63c2
aysebat <aaysebatt@gmail.com>
2023-01-08 17:50:48 +0300

    Update the main.py

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from kivy.app import App
-from kivy.uix.screenmanager import  ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-
+from filesharer import FileSharer
 Builder.load_file('frontend.kv')
+
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.start.text = 'Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.start.text = 'Start Camera'
+        self.ids.camera.texture = None
+    def capture(self):
+        pass
+
+
+class ImageScreenClass(Screen):
+    pass
+
 
 class RootWidget(ScreenManager):
     pass
 
+
 class MainApp(App):
     def build(self):
         return RootWidget()
+
 
 MainApp().run()
